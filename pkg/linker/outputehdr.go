fdfca698b51d3859641567b34144965c37e3acbf
Yang Liu <liuyang22@iscas.ac.cn>
2022-11-28 21:14:15 +0800

    [Lesson-9] Sort output sections

--- pkg/linker/outputehdr.go
+++ pkg/linker/outputehdr.go
@@ -21,6 +21,32 @@ func NewOutputEhdr() *OutputEhdr {
 	}}
 }
 
+func getEntryAddr(ctx *Context) uint64 {
+	for _, osec := range ctx.OutputSections {
+		if osec.Name == ".text" {
+			return osec.Shdr.Addr
+		}
+	}
+	return 0
+}
+
+func getFlags(ctx *Context) uint32 {
+	utils.Assert(len(ctx.Objs) > 0)
+	flags := ctx.Objs[0].GetEhdr().Flags
+	for _, obj := range ctx.Objs[1:] {
+		if obj == ctx.InternalObj {
+			continue
+		}
+
+		if obj.GetEhdr().Flags&EF_RISCV_RVC != 0 {
+			flags |= EF_RISCV_RVC
+			break
+		}
+	}
+
+	return flags
+}
+
 func (o *OutputEhdr) CopyBuf(ctx *Context) {
 	ehdr := &Ehdr{}
 	WriteMagic(ehdr.Ident[:])
@@ -32,12 +58,15 @@ func (o *OutputEhdr) CopyBuf(ctx *Context) {
 	ehdr.Type = uint16(elf.ET_EXEC)
 	ehdr.Machine = uint16(elf.EM_RISCV)
 	ehdr.Version = uint32(elf.EV_CURRENT)
+	ehdr.Entry = getEntryAddr(ctx)
 	// TODO
+	ehdr.ShOff = ctx.Shdr.Shdr.Offset
+	ehdr.Flags = getFlags(ctx)
 	ehdr.EhSize = uint16(EhdrSize)
 	ehdr.PhEntSize = uint16(PhdrSize)
 	// TODO
 	ehdr.ShEntSize = uint16(ShdrSize)
-	// TODO
+	ehdr.ShNum = uint16(ctx.Shdr.Shdr.Size) / uint16(ShdrSize)
 
 	buf := &bytes.Buffer{}
 	err := binary.Write(buf, binary.LittleEndian, ehdr)
